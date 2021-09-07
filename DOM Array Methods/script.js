36bccda5418ddb5e65d4b76480ecd1c3514055d1
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2021-09-07 20:44:10 +0530

    commit

--- DOM Array Methods/script.js
+++ DOM Array Methods/script.js
@@ -1,59 +1,65 @@
-const main = document.getElementById("main");
-const addUserBtn = document.getElementById("addUser");
-const doubleBtn = document.getElementById("double");
-const millionairesBtn = document.getElementById("millionaires");
-const sortBtn = document.getElementById("sort");
-const calculateWealthBtn = document.getElementById("calculateWealth");
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('addUser');
+const doubleBtn = document.getElementById('double');
+const millionairesBtn = document.getElementById('millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculateWealth');
+const clear = document.getElementById('clear');
 
 let data = [],
-    inititalNumberOfUsers = 3;
+    inititalNumberOfUsers = 4;
 
 for (let i = 0; i < inititalNumberOfUsers; ++i)
     getRandomUser();
 
-// fetch random user and add money
+// fetch a new user from api
 async function getRandomUser() {
-    const res = await fetch("https://randomuser.me/api/");
+    const res = await fetch('https://randomuser.me/api/');
     const data = await res.json();
 
     const user = data.results[0];
-
     const newUser = {
         name: `${user.name.first} ${user.name.last}`,
         money: Math.floor(Math.random() * 2000000)
     };
 
-    addData(newUser);
+    addUser(newUser);
 }
 
-// add new obj to data arr
-function addData(obj) {
-    data.push(obj);
+// push a new user into database
+function addUser(user) {
+    data.push(user);
 
     updateDOM();
 }
 
-// update DOM
+// update DOM with provided data
 function updateDOM(providedData = data) {
-    // clear main div
+    // clear main
     main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
 
     providedData.forEach(user => {
-        const element = document.createElement('div');
-        element.classList.add('person');
+        const userElement = document.createElement('div');
+        userElement.classList.add('person');
 
-        element.innerHTML = `<strong>${user.name}</strong> ${formatMoney(user.money)}`;
-
-        main.appendChild(element);
+        userElement.innerHTML = `<strong>${user.name}</strong> ${formatMoney(user.money)}`;
+        main.appendChild(userElement);
     });
 }
 
-// format number as money
+// clear everything in the user list
+function clearMain() {
+    data = [];
+
+    updateDOM();
+}
+
+// format number into money
 function formatMoney(number) {
     return '₹‎' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
 
-// double everyone's money
+// double each user's money
 function doubleMoney() {
     data = data.map(user => {
         return { ...user, money: user.money * 2 };
@@ -62,49 +68,47 @@ function doubleMoney() {
     updateDOM();
 }
 
-// sort data according to money in descending order
-function sortByRichest() {
-    data.sort((a, b) => b.money - a.money);
-
-    updateDOM();
-}
-
-// filter out millionaires
+// filter and display only millionaires
 function showMillionaires() {
     let millionaires = data.filter(user => user.money >= 1000000);
 
-    updateDOM(millionaires);
-}
-
-// toggle filtering only millionaires
-function toggleMillionaires(event) {
     let text = event.target.innerHTML;
-
     if (text === "show only millionares 💵") {
         millionairesBtn.innerHTML = "show all 💵";
-        showMillionaires();
+        updateDOM(millionaires);
     } else {
         millionairesBtn.innerHTML = "show only millionares 💵";
         updateDOM();
     }
 }
 
-// calculate total wealth
-function totalWealth() {
-    const wealth = data.reduce(
+// sort users into descending order of wealth
+function sortByRichest()
+{
+    data = data.sort((a, b) => b.money - a.money);
+
+    updateDOM();
+}
+
+// calculate and display total wealth
+function totalWealth()
+{
+    const total = data.reduce(
         (accumulator, currentValue) => accumulator + currentValue.money
-        , 0);
+    , 0);
 
-    const wealthEl = document.createElement('div');
-    wealthEl.innerHTML = `<h3>Total Wealth: <string>${formatMoney(wealth)}</string></h3>`
+    const totalWealthElement = document.createElement('div');
+    totalWealthElement.innerHTML = `<h3>Total: <strong>${formatMoney(total)}</strong></h3>`;
 
-    main.appendChild(wealthEl);
+    main.appendChild(totalWealthElement);
 }
 
 
 // Event Listeners
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
+millionairesBtn.addEventListener('click', showMillionaires);
 sortBtn.addEventListener('click', sortByRichest);
-millionairesBtn.addEventListener('click', toggleMillionaires);
 calculateWealthBtn.addEventListener('click', totalWealth);
+
+clear.addEventListener('click', clearMain);
